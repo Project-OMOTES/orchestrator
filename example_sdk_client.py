49389081770006902015fe7c1b2ddce58842cba3
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-02-13 16:50:22 +0100

    18: Remove all postgresql stuff, update to sdk 0.0.5, run black on files and various fixes.

--- example_sdk_client.py
+++ example_sdk_client.py
@@ -2,10 +2,40 @@ import base64
 import time
 
 from omotes_sdk.config import RabbitMQConfig
-from omotes_sdk.omotes_interface import OmotesInterface
+from omotes_sdk.omotes_interface import (
+    OmotesInterface,
+    Job,
+    JobResult,
+    JobProgressUpdate,
+    JobStatusUpdate,
+)
 from omotes_sdk.workflow_type import WorkflowType
 
 rabbitmq_config = RabbitMQConfig(username="omotes", password="somepass1", virtual_host="omotes")
+
+
+def handle_on_finished(job: Job, result: JobResult):
+    print(
+        f"Job {job.id} is done (type: {job.workflow_type.workflow_type_name}). "
+        f"Status: {result.result_type}, output esdl length: {len(result.output_esdl)}, "
+        f"logs length: {len(result.logs)}"
+    )
+
+
+def handle_on_status_update(job: Job, status_update: JobStatusUpdate):
+    print(
+        f"Job {job.id} progress (type: {job.workflow_type.workflow_type_name}). "
+        f"Status: {status_update.status}"
+    )
+
+
+def handle_on_progress_update(job: Job, progress_update: JobProgressUpdate):
+    print(
+        f"Job {job.id} progress (type: {job.workflow_type.workflow_type_name}). Progress:"
+        f" {progress_update.progress}, message: {progress_update.message}"
+    )
+
+
 try:
     omotes_if = OmotesInterface(rabbitmq_config)
     omotes_if.start()
@@ -17,10 +47,10 @@ try:
         job_config={},
         workflow_type=WorkflowType("grow_optimizer", "some descr"),
         job_timeout=None,
-        callback_on_finished=print,
-        callback_on_progress_update=print,
-        callback_on_status_update=print,
+        callback_on_finished=handle_on_finished,
+        callback_on_progress_update=handle_on_progress_update,
+        callback_on_status_update=handle_on_status_update,
     )
-    time.sleep(30)
+    time.sleep(60)
 finally:
     omotes_if.stop()
--- src/omotes_orchestrator/celery_interface.py
+++ src/omotes_orchestrator/celery_interface.py
@@ -8,13 +8,18 @@ from omotes_orchestrator.config import CeleryConfig
 
 
 class CeleryInterface:
+    """Connect to the Celery app which orchestrates the workers."""
+
     app: Celery
+    """The Celery app through which tasks may be monitored & started."""
     config: CeleryConfig
+    """Config for the Celery app."""
 
     def __init__(self, config: CeleryConfig) -> None:
         self.config = config
 
     def start(self) -> None:
+        """Start the Celery app."""
         rabbitmq_config = self.config.rabbitmq_config
 
         self.app = Celery(
@@ -23,11 +28,19 @@ class CeleryInterface:
         )
 
     def stop(self) -> None:
+        """Stop the Celery app."""
         self.app.close()
 
     def start_workflow(
         self, workflow_type: WorkflowType, job_id: uuid.UUID, input_esdl: bytes
     ) -> None:
+        """Start a new workflow.
+
+        :param workflow_type: Type of workflow to start. Currently this translates directly to
+            a Celery task with the same name.
+        :param job_id: The OMOTES ID of the job.
+        :param input_esdl: The ESDL to perform the task on.
+        """
         self.app.signature(
             workflow_type.workflow_type_name,
             (job_id, input_esdl),
