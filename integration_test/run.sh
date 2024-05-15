dd2a5a42fb95b1de6fadca425e40dd9c5cb60a5b
Sebastiaan la Fleur <sebastiaan.lafleur@tno.nl>
2024-05-15 10:45:52 +0200

    35: Rename src/ to job_submitter/ in integration_test/

--- integration_test/run.sh
+++ integration_test/run.sh
@@ -4,8 +4,8 @@ export LOG_LEVEL=WARNING
 
 if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then # Linux
   . .venv/bin/activate
- python3 src/job_submitter.py
+ python3 job_submitter/job_submitter.py
 else
   source venv/Scripts/activate
-  python src/job_submitter.py
+  python job_submitter/job_submitter.py
 fi
