ed438c8db788e2acd03b3c59f8f945241d7d92ed
Sebastiaan la Fleur <sebastiaan@lfse.nl>
2025-01-09 14:02:35 +0100

    87: Add debug info commands to lint temporarily.

--- ci/linux/lint.sh
+++ ci/linux/lint.sh
@@ -5,4 +5,12 @@ if [[ "$OSTYPE" != "win32" && "$OSTYPE" != "msys" ]]; then
   . .venv/bin/activate
 fi
 
+
+which python
+which python3
+ls -alh .
+echo $PWD
+python --version
+python3 --version
+
 flake8 ./src/omotes_orchestrator ./unit_test/
